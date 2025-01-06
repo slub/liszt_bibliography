b36764c23c70b3af0f396b24034d39c744166ce4
thomas-sc <info@schefter.net>
2025-01-06 17:08:23 +0100

    debug mapping and indexing (index only 3 docs)

--- Classes/Processing/BibElasticMapping.php
+++ Classes/Processing/BibElasticMapping.php
@@ -53,15 +53,22 @@ class BibElasticMapping
                             ]
                         ],
                         'fulltext' => [ 'type' => 'text' ],
-                        BibEntryProcessor::HEADER_FIELD => [ 'type' => 'text' ],
-                        BibEntryProcessor::BODY_FIELD => [ 'type' => 'text' ],
-                        BibEntryProcessor::FOOTER_FIELD => [ 'type' => 'text' ],
-                        BibEntryProcessor::SEARCHABLE_FIELD => ['type' => 'text', 'copy_to' => 'fulltext'],
-                        BibEntryProcessor::BOOSTED_FIELD => ['type' => 'text'],
-                        BibEntryProcessor::CREATORS_FIELD => [
+                        'tx_lisztcommon_header' => [ 'type' => 'text' ],
+                        'tx_lisztcommon_body' => [ 'type' => 'text' ],
+                        'tx_lisztcommon_footer' => [ 'type' => 'text' ],
+                        'tx_lisztcommon_searchable' => ['type' => 'text', 'copy_to' => 'fulltext'],
+                        'tx_lisztcommon_boosted' => ['type' => 'text'],
+                        'tx_lisztcommon_creators' => [
                             'type' => 'nested',
                             'properties' => [
-                                'fullName' => ['type' => 'text'],
+                                'fullName' => [
+                                    'type' => 'text',
+                                    'fields' => [
+                                        'keyword' => [
+                                            'type' => 'keyword', 'ignore_above' => 256
+                                        ],
+                                    ],
+                                ],
                             ]
                         ],
                     ]
